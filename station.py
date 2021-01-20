dbd97341479301131e385db4a59307210def2e98
kimsifers <34680912+kimsifers@users.noreply.github.com>
2021-01-20 23:45:05 +0200

    Update and rename station20201205.txt to station.py
    2021.01.20 version

--- station.py
+++ station.py
@@ -27,7 +27,7 @@ class Station(Producer):
             self.name.lower()
             .replace("/", "_and_")
             .replace(" ", "_")
-            .replace("-", "_")
+                       .replace("-", "_")
             .replace("'", "")
         )
 
@@ -41,15 +41,18 @@ class Station(Producer):
         #the technical limitations.  It seem like with 8 stations and 12 arrivals an hour 
         #we should need to process 10 mb per topic X 8 Stations # 12 arrivals X 60 seconds 
         #x 60 minutes = 34 560 000 mb per hour 
-        topic_name = f"CTAProducersArrivals{self.name}" # TODO: Come up with a better topic name, better to use self.name so that it matches with source 
+        #topic_name = topic name principals, Business Name Chicago Transport Authority (CTA), Name of the Python program, Name of the class # TODO: Come up with a better topic name
+        topic_name = "CTAProducersStation"
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
             value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            num_partitions=2,
-            num_replicas=1,
+            num_partitions=1,
+            num_replicas=1,  
         )
 
+        
+        #self.topic.name = topic_name 
         self.station_id = int(station_id)
         self.color = color
         self.dir_a = direction_a
@@ -57,41 +60,55 @@ class Station(Producer):
         self.a_train = None
         self.b_train = None
         self.turnstile = Turnstile(self)
-
+        self.line = color.name
+        #self.train_status = train_status.name
+        
+   
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
-     	while Rrue: 
+        
+
+    
         #
         #
         # TODO: Complete this function by producing an arrival message to Kafka
         #
-        # I defined the fields that had been defined in Class Station 
-        logger.info("arrival kafka integration incomplete - skipping")
-        self.producer.produce(
-            topic=self.topic_name,
-            key={"timestamp": self.time_millis()},
-            value={"station_id": self.station_id,
-                  "line":self.color,
-                  "direction": self.dir_a,
-                   "prev_direction":self.dir_b},
-        )
+        # call the producer.produce class, try to find errors, 
+        # for testing purposes printed successful processing 
+        
+        
+        
+        
+    
+         
+        try: 
+            self.producer.produce(  
+                topic=self.topic_name,
+                key={"timestamp": self.time_millis()},
+                value={
+                    "station_id": self.station_id,
+                    "train_id": train.train_id,
+                    "direction": direction,
+                    "line": self.line,
+                    "train_status": train.status.name,
+                    "prev_station_id": prev_station_id,
+                    "prev_direction": prev_direction,}
+                    )
+            
+        except Exception as e:
+            logger.info("arrival kafka integration incomplete - skipping")
+            print(f"Exception while producing record value to topic - {self.topic_name}: {e}")
+        #else:
+        #    print(f"Successfully producing record value to topic - {self.topic_name}")
+        
+        
+            
 
- #from turnstiles
- # self.producer.produce(
- #               topic=self.topic_name,
- #               key={"timestamp": self.time_millis()},
- #               value={
- #                   "station_id": self.station.station_id,
- #                   "station_name": self.station.name,
- #                   "line": self.station.color.name
- #                   #"num_entries": num_entries
- #               }
- #           )
-
-
+            
 
     def __str__(self):
+        print("station after _str_ line 101")
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
             self.station_id,
             self.name,
@@ -118,3 +135,5 @@ class Station(Producer):
         """Prepares the producer for exit by cleaning up the producer"""
         self.turnstile.close()
         super(Station, self).close()
+
+
